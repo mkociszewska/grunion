304e4e1f00ba10a673490c2efac89ddb33ac5c22
Kociamber <koci@o2.pl>
2016-04-28 17:54:05 +0200

    remove registration links and make it less obvious ;)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  devise_scope :user do
+    get "/noteveryonecansignup" => "devise/registrations#new"
+    # But every hax0r will be able to see it on github! Pretty cool, isn't it? ;)
+  end
+
   root 'articles#index'
 
   resources :articles
